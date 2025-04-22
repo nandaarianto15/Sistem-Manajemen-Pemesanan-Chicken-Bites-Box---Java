1429920628430187151be7f2a1e13f005dc7cbd7
Nanda Arianto <102655772+nandaarianto15@users.noreply.github.com>
2025-04-22 11:03:15 +0800

    Upload Posttest 5

---
+++ posttest5/src/model/Minuman.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Minuman extends Menu {
+    private boolean isDingin;
+
+    public Minuman(int id, String nama, String ukuran, double harga, boolean isDingin) {
+        super(id, nama, ukuran, harga); // Memanggil konstruktor dari class Menu
+        this.isDingin = isDingin;
+    }
+
+    public boolean isDingin() {
+        return isDingin;
+    }
+
+    // Override
+    @Override
+    public String toString() {
+        return super.toString() + " | Dingin: " + (isDingin ? "Iya" : "Tidak");
+    }
+
+    @Override
+    public String getJenisMenu() {
+        return "Minuman";
+    }
+}
