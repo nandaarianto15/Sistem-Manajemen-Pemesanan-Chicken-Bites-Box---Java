c23e05323d555d4c89c4d05e2dd44dce23488de0
Nanda Arianto <102655772+nandaarianto15@users.noreply.github.com>
2025-04-22 10:57:55 +0800

    Upload Posttest 5

---
+++ posttest5/src/model/Pesanan.java
@@ -0,0 +1,28 @@
+package model;
+
+// FINAL CLASS - untuk mencegah class lain mewarisinya
+public final class Pesanan {
+    private int id;
+    private String telp;
+    private String email;
+    private Menu menu;
+    private int jumlah;
+
+    public Pesanan(int id, String telp, String email, Menu menu, int jumlah) {
+        this.id = id;
+        this.telp = telp;
+        this.email = email;
+        this.menu = menu;
+        this.jumlah = jumlah;
+    }
+
+    @Override
+    public String toString() {
+        return "ID Pesanan: " + id +
+               " | Telp: " + telp +
+               " | Email: " + email +
+               " | Menu: " + menu.getNama() +
+               " | Jumlah: " + jumlah +
+               " | Total: Rp" + String.format("%,.0f", jumlah * menu.getHarga());
+    }
+}
